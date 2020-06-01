669fb2b9cf03eacc54bdfce545f25fa8c4fa2ae3
Andrey Fedinyak <fedinyakandrey@gmail.com>
2020-06-02 02:33:28 +0300

    make statistic window

---
+++ js/stat.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 120;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var VICTORY_TEXT_HEIGHT = 60;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function(ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, "rgba(0, 0, 0, 0.3)");
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, "#fff");
+
+  ctx.fillStyle = "#000";
+
+  ctx.font = "16px PT Mono";
+  ctx.textBaseline = "hanging";
+  ctx.fillText("Ура вы победили!", CLOUD_X + 20, CLOUD_Y + 20);
+  ctx.fillText("Список результатов:", CLOUD_X + 20, CLOUD_Y + 40);
+
+  var maxTime = getMaxElement(times);
+
+  var getRandomSaturation = function() {
+    return "hsl(255, " + getRandomNumber(100) + "%, 50%)";
+  };
+
+  function getRandomNumber(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = "#000";
+    ctx.fillText(
+      players[i],
+      CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
+      CLOUD_HEIGHT - FONT_GAP
+    );
+    ctx.fillText(
+      Math.round(times[i]),
+      CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
+      CLOUD_Y + GAP * 6 + BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime
+    );
+
+    ctx.fillStyle =
+      players[i] === "Вы" ? "rgba(255, 0, 0, 1)" : getRandomSaturation();
+
+    ctx.fillRect(
+      CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
+      CLOUD_Y +
+        BAR_GAP +
+        GAP * 3 +
+        (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime),
+      BAR_WIDTH,
+      (BAR_HEIGHT * times[i]) / maxTime
+    );
+  }
+};
