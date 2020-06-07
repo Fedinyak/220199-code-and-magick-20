abd7651b87cb7090efaf051e7ad36caab8d3b8cd
Andrey Fedinyak <fedinyakandrey@gmail.com>
2020-06-07 21:33:48 +0300

    add var

--- js/stat.js
+++ js/stat.js
@@ -5,12 +5,18 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 120;
 var CLOUD_Y = 10;
 var GAP = 10;
+var FONT_FAMILY = '16px PT Mono';
+var FONT_ALIGN = 'hanging';
 var FONT_GAP = 15;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var TEXT_VICTORY = 'Ура вы победили!';
 var TEXT_STAT = 'Список результатов:';
+var COLOR_CLOUD = '#fff';
+var COLOR_SHADOW = 'rgba(0, 0, 0, 0.3)';
+var COLOR_BLACK = '#000';
+var COLOR_RED = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -38,20 +44,20 @@ function getRandomNumber(max) {
 }
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, COLOR_SHADOW);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, COLOR_CLOUD);
 
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = COLOR_BLACK;
 
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
+  ctx.font = FONT_FAMILY;
+  ctx.textBaseline = FONT_ALIGN;
   ctx.fillText(TEXT_VICTORY, CLOUD_X + 20, CLOUD_Y + 20);
   ctx.fillText(TEXT_STAT, CLOUD_X + 20, CLOUD_Y + 40);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = COLOR_BLACK;
     ctx.fillText(
         players[i],
         CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
@@ -64,7 +70,7 @@ window.renderStatistics = function (ctx, players, times) {
     );
 
     ctx.fillStyle =
-      players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getRandomSaturation();
+      players[i] === 'Вы' ? COLOR_RED : getRandomSaturation();
 
     ctx.fillRect(
         CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
